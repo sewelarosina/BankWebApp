71fd95456856c139e55cee810374f72163a868c9
sewelarosina <kseakamela08@gmail.com>
2026-01-15 10:12:10 +0200

    replacing the bankcontroller.cs and transaction.cs codes

--- BankWebApp/Controllers/BankController.cs
+++ BankWebApp/Controllers/BankController.cs
@@ -1,37 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using YourProjectName.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using YourProjectName.Models;
 
 namespace YourProjectName.Controllers
 {
     public class BankController : Controller
     {
         private static List<BankAccount> accounts = new();
+        private static List<Transaction> transactions = new();
+        private static int transactionIdCounter = 1;
 
         public IActionResult Index()
         {
             return View(accounts);
         }
 
+        // Create Account GET
         public IActionResult Create()
         {
             return View();
         }
 
+        // Create Account POST
         [HttpPost]
-        public IActionResult Create(BankAccount account)
+        public IActionResult Create(BankAccount model)
         {
-            account.AccountNumber = accounts.Count + 1001;
-            accounts.Add(account);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                accounts.Add(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
 
+        // Transfer GET
         public IActionResult Transfer()
         {
             return View();
         }
 
+        // Transfer POST
         [HttpPost]
         public IActionResult Transfer(TransferViewModel model)
         {
@@ -47,14 +57,40 @@ namespace YourProjectName.Controllers
             from.Balance -= model.Amount;
             to.Balance += model.Amount;
 
+            transactions.Add(new Transaction
+            {
+                Id = transactionIdCounter++,
+                AccountNumber = from.AccountNumber,
+                Type = TransactionType.Transfer,
+                Amount = model.Amount,
+                Date = DateTime.Now,
+                RelatedAccountNumber = to.AccountNumber
+            });
+
+            transactions.Add(new Transaction
+            {
+                Id = transactionIdCounter++,
+                AccountNumber = to.AccountNumber,
+                Type = TransactionType.Transfer,
+                Amount = model.Amount,
+                Date = DateTime.Now,
+                RelatedAccountNumber = from.AccountNumber
+            });
+
             return RedirectToAction("Index");
         }
 
+        // Deposit GET
         public IActionResult Deposit(int accountNumber)
         {
+            var account = accounts.FirstOrDefault(a => a.AccountNumber == accountNumber);
+            if (account == null)
+                return NotFound();
+
             return View(new AmountViewModel { AccountNumber = accountNumber });
         }
 
+        // Deposit POST
         [HttpPost]
         public IActionResult Deposit(AmountViewModel model)
         {
@@ -67,14 +103,30 @@ namespace YourProjectName.Controllers
             }
 
             account.Balance += model.Amount;
+
+            transactions.Add(new Transaction
+            {
+                Id = transactionIdCounter++,
+                AccountNumber = account.AccountNumber,
+                Type = TransactionType.Deposit,
+                Amount = model.Amount,
+                Date = DateTime.Now
+            });
+
             return RedirectToAction("Index");
         }
 
+        // Withdraw GET
         public IActionResult Withdraw(int accountNumber)
         {
+            var account = accounts.FirstOrDefault(a => a.AccountNumber == accountNumber);
+            if (account == null)
+                return NotFound();
+
             return View(new AmountViewModel { AccountNumber = accountNumber });
         }
 
+        // Withdraw POST
         [HttpPost]
         public IActionResult Withdraw(AmountViewModel model)
         {
@@ -87,7 +139,35 @@ namespace YourProjectName.Controllers
             }
 
             account.Balance -= model.Amount;
+
+            transactions.Add(new Transaction
+            {
+                Id = transactionIdCounter++,
+                AccountNumber = account.AccountNumber,
+                Type = TransactionType.Withdraw,
+                Amount = model.Amount,
+                Date = DateTime.Now
+            });
+
             return RedirectToAction("Index");
+        }
+
+        // Transaction History
+        public IActionResult Transactions(int accountNumber)
+        {
+            var account = accounts.FirstOrDefault(a => a.AccountNumber == accountNumber);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            var accountTransactions = transactions
+                .Where(t => t.AccountNumber == accountNumber)
+                .OrderByDescending(t => t.Date)
+                .ToList();
+
+            ViewBag.Account = account;
+            return View(accountTransactions);
         }
     }
 }
--- BankWebApp/Models/Transaction.cs
+++ BankWebApp/Models/Transaction.cs
@@ -1,6 +1,21 @@
-﻿namespace BankWebApp.Models
+﻿using System;
+
+namespace YourProjectName.Models
 {
+    public enum TransactionType
+    {
+        Deposit,
+        Withdraw,
+        Transfer
+    }
+
     public class Transaction
     {
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public int? RelatedAccountNumber { get; set; }
     }
 }
