c00d04316aafbba89f2f06df9f83c5315db81264
sewelarosina <kseakamela08@gmail.com>
2026-01-23 08:38:33 +0200

    created a data folder that will contain the appplicationDbcontext  and updated a code inside

---
+++ BankWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BankingApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using YourProjectName.Models;
+
+namespace BankingApp.Data
+{
+    public class ApplicationDbContext
+        : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ BankWebApp/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BankingApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Extend later (e.g. FullName)
+    }
+}
